5663d1d655f2d15c00826e807571979fa35c3b9e
elijahdunbar <eli.dunbar18@gmail.com>
2024-10-07 10:12:29 -0400

    figuring out venv

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Silly Snek")
+screen.tracer(0)
+#Create and move a paddle
+#Create another Paddle
+#Create the ball and make it move
+#Detect collision with wall and bounce
+#Detect collision with paddle
+#Detect when paddle misses
+#Keep score
